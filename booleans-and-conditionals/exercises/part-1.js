304555ae135caa6e9e3e72f5befc30dbc8cc692e
unknown <jp.heidbrink@gmail.com>
2024-06-15 15:29:04 -0500

    modified exercises for booleans-and-conditionals

--- booleans-and-conditionals/exercises/part-1.js
+++ booleans-and-conditionals/exercises/part-1.js
@@ -1,4 +1,10 @@
 // Declare and initialize the variables for exercise 1 here:
+let engineIndicatorLight = "green";
+let spaceSuitsOn = true;
+let shuttleCabinReady = true;
+let crewStatus = spaceSuitsOn && shuttleCabinReady;
+let computerStatusCode = 200;
+let shuttleSpeed = 15000;
 
 // BEFORE running the code, predict what will be printed to the console by the following statements:
 
