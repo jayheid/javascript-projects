12d9cdefd563ae983945526898e7a074a7efd6e8
unknown <jp.heidbrink@gmail.com>
2024-07-07 19:48:39 -0500

    modified studio

--- more-on-functions/studio/part-two-create-sorted-array.js
+++ more-on-functions/studio/part-two-create-sorted-array.js
@@ -25,33 +25,17 @@ function sortArray(numArray){
 
   for (let i = 0; i < counter; i++){
     if (numArray.length > 1){
-      console.log(`iteration: ${i+1}`);
-
-      console.log(`Num Array: ${numArray}`);
-
       let minValue = findMinValue(numArray);
-      console.log(`min value: ${minValue}`);
       sortedArray.push(minValue);
-      
-      console.log(`Splicing: ${numArray.indexOf(minValue)}`);
-      numArray.splice(numArray.indexOf(minValue), 1);
-      
-      console.log(`Num Array: ${numArray}`);
+      numArray.splice(numArray.indexOf(minValue), 1);    
     }else{
-      console.log(`min value: ${numArray[0]}`);
       sortedArray.push(numArray[0]);
-      
-      console.log(`Splicing: ${numArray[0]}`);
       numArray.splice(0, 1);
-      
     }
-
   }
 
   return sortedArray;
 }
-
-
 
 /* BONUS MISSION: Refactor your sorting function to use recursion below:
  */
@@ -61,4 +45,25 @@ let nums1 = [5, 10, 2, 42];
 let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
 let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
 
-console.log(sortArray(nums1));
+// console.log(sortArray(nums1));
+
+function sortArray2(numArray){
+  let minValue;
+
+  if (numArray.length === 1){
+    minValue = numArray[0];
+    numArray.splice(numArray.indexOf(minValue), 1); // remove item at current index
+    return numArray.push(minValue)
+  }
+  else {
+    minValue = findMinValue(numArray); // find minimum value
+    numArray.splice(numArray.indexOf(minValue), 1); // remove item at current index
+    numArray.push(minValue); // add item to end of list
+
+    return sortArray2(numArray.splice(0, numArray.length - 1)); // repeat function for items before added item 
+  }
+}
+
+console.log(sortArray2(nums1));
+
+// create new array in recursive function?
