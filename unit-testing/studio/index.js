d6d70792dee3f97c51691fbcbaf8a8ce6fda121b
unknown <jp.heidbrink@gmail.com>
2024-07-16 17:42:34 -0500

    adding studio

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,27 @@
-
 let launchcode = {
-  
-}
+  organization: "nonprofit",
+  executiveDirector: "Jeff",
+  percentageCoolEmployees: 100,
+  programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+  launchOutput: function (num) {
+
+    if (num / 2 === 1) {
+      return "Launch!";
+    } else if (num / 3 === 1) {
+      return "Code!";
+    } else if (num / 5 === 1) {
+      return "Rocks!";
+    } else if (num % 2 === 0 && num % 3 === 0 && num % 5 !== 0) {
+      return "LaunchCode!";
+    } else if (num % 3 === 0 && num % 5 === 0 && num % 2 !== 0) {
+      return "Code Rocks!";
+    } else if (num % 2 === 0 && num % 5 === 0 && num % 3 !== 0) {
+      return "Launch Rocks!";
+    } else if (num % 2 === 0 && num % 3 === 0 && num % 5 === 0) {
+        return "LaunchCode Rocks!";
+    }
+
+  },
+};
 
 module.exports = launchcode;
-
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -1,8 +1,54 @@
 // launchcode.test.js code:
-const launchcode = require('../index.js');
+const launchcode = require("../index.js");
 
-describe("Testing launchcode", function(){
+describe("Testing launchcode", function () {
+  test("check organization type", function () {
+    expect(launchcode.organization).toEqual("nonprofit");
+  });
 
-  // Write your unit tests here!
+  test("check executive director name", function () {
+    expect(launchcode.executiveDirector).toBe("Jeff");
+  });
+
+  test("check percentage cool employees", function () {
+    expect(launchcode.percentageCoolEmployees).toEqual(100);
+  });
+
+  test("check programs offered", function () {
+    expect(launchcode.programsOffered).toEqual([
+      "Web Development",
+      "Data Analysis",
+      "Liftoff",
+    ]);
+  });
+
+  test("check launch output method for number only divisible by 2", function () {
+    expect(launchcode.launchOutput(2)).toEqual("Launch!");
+  });
+
+  test("check launch output method for number only divisible by 3", function () {
+    expect(launchcode.launchOutput(3)).toEqual("Code!");
+  });
+
+  test("check launch output method for number only divisible by 5", function () {
+    expect(launchcode.launchOutput(5)).toEqual("Rocks!");
+  });
+
+  test("check launch output method for number only divisible by 2 and 3", function () {
+    expect(launchcode.launchOutput(6)).toEqual("LaunchCode!");
+  });
+
+  test("check launch output method for number only divisible by 3 and 5", function () {
+    expect(launchcode.launchOutput(15)).toEqual("Code Rocks!");
+  });
   
+  test("check launch output method for number only divisible by 2 and 5", function () {
+    expect(launchcode.launchOutput(10)).toEqual("Launch Rocks!");
+  });
+
+  test("check launch output method for number only divisible by 2, 3 and 5", function () {
+    expect(launchcode.launchOutput(30)).toEqual("LaunchCode Rocks!");
+  });
+
+
 });
