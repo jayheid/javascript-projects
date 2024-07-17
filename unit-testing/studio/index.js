acef2f29e1a0a346d70cbe9513bb7a0480cbaffa
unknown <jp.heidbrink@gmail.com>
2024-07-16 20:19:20 -0500

    modified studio

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -4,23 +4,19 @@ let launchcode = {
   percentageCoolEmployees: 100,
   programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
   launchOutput: function (num) {
+    outputArray = ['Launch','Code',' Rocks','!'];
+    outputString = '';
 
-    if (num / 2 === 1) {
-      return "Launch!";
-    } else if (num / 3 === 1) {
-      return "Code!";
-    } else if (num / 5 === 1) {
-      return "Rocks!";
-    } else if (num % 2 === 0 && num % 3 === 0 && num % 5 !== 0) {
-      return "LaunchCode!";
-    } else if (num % 3 === 0 && num % 5 === 0 && num % 2 !== 0) {
-      return "Code Rocks!";
-    } else if (num % 2 === 0 && num % 5 === 0 && num % 3 !== 0) {
-      return "Launch Rocks!";
-    } else if (num % 2 === 0 && num % 3 === 0 && num % 5 === 0) {
-        return "LaunchCode Rocks!";
-    }
+    if (num % 2 === 0) {
+      outputString+=outputArray[0];
+    } if (num % 3 === 0) {
+      outputString += outputArray[1];
+    } if (num % 5 === 0) {
+      outputString += outputArray[2];
+    } 
 
+    outputString += outputArray[3];
+    return outputString.trim()
   },
 };
 
